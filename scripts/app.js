e1e839fce0d69e62e4852f16f61658797e171eb1
DParekh0409 <darpanparekh04@gmail.com>
2025-08-10 23:19:47 -0400

    Tried

---
+++ scripts/app.js
@@ -0,0 +1,70 @@
+console.log("JS Loaded")
+const app= Vue.createApp({
+    data() {
+        return {
+            randUser: {
+                Name: "",
+                Age: "",
+                Avatar: "",
+            },
+            randWeather: {
+                City: "London",
+                Province: "ON",
+                Country: "CAN",
+                Temp: "",
+                Wind: "",
+                Description: "",
+            },
+            randDictionary: {
+                Word: "",
+                Phonetic: "",
+                Definition: "",
+            },
+        }
+    },
+    methods: {
+        fetchrandUser() {
+            fetch("https://comp6062.liamstewart.ca/random-user-data")
+            .then(response => {
+                if (response.ok) {
+                    return response.json();
+                }
+                else {
+                    console.log("Error! Try Again");
+                }
+            })
+            .then(data => {
+                this.randUser= {
+                    Name: `${data.fullName}`,
+                    Age: data.Age,
+                    Avatar: data.avatar_url,
+                }
+            })
+            .catch(error => {
+                console.log("Failure");
+            });
+        },
+        fetchrandWeather() {
+            fetch("https://comp6062.liamstewart.ca/weather-data")
+            .then(response => {
+                if (response.ok) {
+                    return response.json();
+                }
+                else {
+                    console.log("Error! Try Again");
+                }
+            })
+            .then(data => {
+                this.randWeather= {
+                    Temperature: `${data.currentTemp}`,
+                    Wind: `${data.currentWind}`,
+                    Description: `${data.currentCondition}`,
+                }
+            })
+            .catch(error => {
+                console.log("Failure");
+            });
+        },
+    },
+});
+app.mount("#app");
